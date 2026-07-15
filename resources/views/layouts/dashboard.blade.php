14e9cf331c57dddd1bc120657470c11026736e62
leng <leng@lengs-MacBook-Pro.local>
2026-07-15 20:27:16 +0700

    fixed responsive sidebar

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -25,13 +25,19 @@
 
     <div class="flex min-h-screen w-full overflow-x-hidden">
 
+    <div class="flex min-h-screen w-full overflow-x-hidden">
+
+        @include('partials.sidebar')
         @include('partials.sidebar')
 
-    <div id="main-content">
-        @yield('content')
+        <div id="main-content" class="flex-1 min-w-0 p-6 transition-all duration-300">
+            @yield('content')
+        </div>
+
     </div>
+
 @stack('scripts')
-<script src="https://cdn.jsdelivr.net/gh/ThyrithSor/momentkh@3.0.3/momentkh.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</body>
 </body>
 </html>
